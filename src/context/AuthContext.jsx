9710519076c7e4093cf7256a97abb6eac1be387b
BTavkhelidze <125907215+BTavkhelidze@users.noreply.github.com>
2025-11-01 23:44:31 +0400

    Integrate React Query for authentication state
    Replaces local user state with React Query to manage authentication state using fetchCurrentUser, loginUser, and logoutUser API functions. Adds login and logOut methods, and exposes loading and error states in the AuthContext.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,12 +1,45 @@
 import { createContext, useContext, useState } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { fetchCurrentUser, loginUser, logoutUser } from '../api/auth';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState('hello');
+  const queryClient = useQueryClient();
+  //auth me response
+  const {
+    data: user,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['auth', 'me'],
+    queryFn: fetchCurrentUser,
+    retry: false,
+    refetchOnWindowFocus: false,
+  });
 
+  const login = async (credential) => {
+    await loginUser(credential);
+    await queryClient.invalidateQueries(['auth', 'me']);
+  };
+
+  const logOut = async (credential) => {
+    await logoutUser(credential);
+
+    await queryClient.setQueriesData(['auth', 'me'], null);
+
+    await queryClient.removeQueries(['auth', 'me']);
+  };
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuthLoading: isLoading,
+        isAuthError: isError,
+        login,
+        logOut,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
@@ -17,6 +50,7 @@ export const AuthProvider = ({ children }) => {
 // eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
   const ctx = useContext(AuthContext);
+
   if (!ctx) throw new Error('useAuth must be used within Provider');
 
   return ctx;
